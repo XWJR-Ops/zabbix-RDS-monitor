7920975c6ff91931f63263ddb8176576d8e42a35
X-Mars <xtlyk@126.com>
2017-08-11 17:17:22 +0800

    Update discovery_rds.py

--- discovery_rds.py
+++ discovery_rds.py
@@ -26,7 +26,7 @@ def GetRdsList():
             DBInstanceIdList.append(DBInstanceIdDict)
         except Exception, e:
             print Exception, ":", e
-            pass
+            
 
 
 GetRdsList()
